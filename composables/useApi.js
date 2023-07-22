25e7332acbbecd1c1ca93addcaabcf5c0c346b9c
Enzo Hideo <enzohideo@gmail.com>
2023-07-22 07:05:06 -0300

    feat(pages): add exit page
    This displays a "you can close this window" message and sends a request
    to the api to exit the game.

--- composables/useApi.js
+++ composables/useApi.js
@@ -1,4 +1,5 @@
 const BASE_URL = "http://localhost:5000/v1/"
+const EXIT_URL = "http://localhost:5000/exit"
 
 export async function $get(endpoint, metadata) {
   return useFetch(endpoint, {
@@ -17,4 +18,8 @@ export async function $post(endpoint, body, metadata) {
     initialCache: false,
     ...metadata
   });
+}
+
+export async function $exit() {
+  return useFetch(EXIT_URL);
 }
--- pages/exit.vue
+++ pages/exit.vue
@@ -1,11 +1,17 @@
 <template>
-
+  <div class="center">
+    <h3>You exited the game.</h3>
+    <h3>You can now close this window.</h3>
+  </div>
 </template>
 
 <script setup>
-
+  $exit();
 </script>
 
 <style scoped>
-
+  h3 {
+    margin: 15px;
+    font-size: 30px;
+  }
 </style>
