5ca66d1a802be015e47371f933fa9155e11dc4f3
Enzo Hideo <enzohideo@gmail.com>
2023-07-22 06:06:12 -0300

    refactor(pages/index): reorganize css and html
    Keep the same appearance while fixing small positioning anomalies. More
    specifically, fix the distance between the title and buttons so they
    don't overlap, center the menu roughly in the center of the screen, and
    swap the <ul> list with a <div>.

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,11 @@
 <template>
-  <div>
-
-    <h2> GALAXY &nbsp;RAIDERS </h2>
-
-    <ul>
-      <li><NuxtLink class="button" to="/game">Start</NuxtLink></li>
-      <li><NuxtLink class="button" to="/leaderboard">Leaderboard</NuxtLink></li>
-      <li><NuxtLink class="button" to="/exit">Exit</NuxtLink></li>
-    </ul>
-
+  <div class="menu">
+    <h2>Galaxy Raiders</h2>
+    <div class="button-wrapper">
+      <NuxtLink class="button" to="/game">Start</NuxtLink>
+      <NuxtLink class="button" to="/leaderboard">Leaderboard</NuxtLink>
+      <NuxtLink class="button" to="/exit">Exit</NuxtLink>
+    </div>
   </div>
 </template>
 
@@ -17,17 +14,27 @@
 </script>
 
 <style>
-  h2 {
-    font-family: serif; 
-    color: white;
-    margin-bottom: 30px;
-    font-size: 75px;
+  :root {
+    --button-shadow: 0px 10px 0px #1672b5, 0px 10px 30px #1672b5;
+  }
+
+  .menu {
     text-align: center;
-    font-weight: bold;
     position: absolute;
-    top: 10%;
-    width: 100%;
+    top: 45%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+  }
+
+  h2 {
+    margin-bottom: 30px;
+
+    font-family: serif;
+    font-weight: bold;
+    font-size: 75px;
+
     animation: color-change 4s infinite;
+
     text-shadow: 
     0 1px 0 hsl(174,5%,80%),
     0 2px 0 hsl(174,5%,75%),
@@ -46,24 +53,33 @@
   }
 
   .button {
-    position: relative;
-    top: 140px;
-    color: white;
-    width: 300px;
-    text-decoration: none;
-    background-color: none;
-    font-weight: bold;
-    font-size: 40px;
     display: block;
-    padding: 10px;
-    border-radius: 10px;
-    -webkit-border-radius: 30px;
-    -moz-border-radius: 10px;
-    box-shadow: 0px 10px 0px #1672b5, 0px 10px 30px #1672b5;
-    -webkit-box-shadow: 0px 10px 0px #1672b5, 0px 10px 30px #1672b5;
-    -moz-box-shadow: 0px 10px 0px #1672b5, 0px 10px 30px #1672b5;
-    margin: 30px auto;
     text-align: center;
+
+    position: relative;
+    width: 100%;
+
+    color: white;
+    background-color: transparent;
+
+    font-family: serif;
+    font-weight: bold;
+    font-size: 30px;
+
+    margin: 30px auto;
+    padding: 25px 40px 25px 40px;
+    box-sizing: border-box;
+
+    text-decoration: none;
+
+    border-radius: 30px;
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+
+    box-shadow: var(--button-shadow);
+    -webkit-box-shadow: var(--button-shadow);
+    -moz-box-shadow: var(--button-shadow);
+
     transition: all .2s ease;
     -webkit-transition: all .2s ease;
     -moz-transition: all .2s ease;
@@ -73,23 +89,25 @@
 
   .button:active {
     position: relative;
-    top: 145px;
-    -webkit-box-shadow: 0px 3px 0px #095e9c, 0px 9px 25px #095e9c;
-    -moz-box-shadow: 0px 3px 0px #095e9c, 0px 9px 25px #095e9c;
-    box-shadow: 0px 3px 0px #095e9c, 0px 9px 25px #095e9c;
+    transform: translateY(5px);
+    box-shadow: unset;
+  }
+
+  .button-wrapper {
+    display: inline-block;
   }
 
   @keyframes color-change {
-    0% { color: light-blue; }
-    10% { color: gray; }
-    20% { color: white; }
-    30% { color: yellow; }
-    40% { color: orange; }
-    50% { color: pink; }
-    60% { color: orange; }
-    70% { color: yellow; }
-    80% { color: white; }
-    90% { color: gray; }
-    100% { color: silver; }
+    0%    { color: blue   ; }
+    10%   { color: gray   ; }
+    20%   { color: white  ; }
+    30%   { color: yellow ; }
+    40%   { color: orange ; }
+    50%   { color: pink   ; }
+    60%   { color: orange ; }
+    70%   { color: yellow ; }
+    80%   { color: white  ; }
+    90%   { color: gray   ; }
+    100%  { color: silver ; }
   }
 </style>
